a2dc9dd2d45b670fb3463f3aa8efe826c3dde8f8
radium226 <adrien@adrien-ux31a.localdomain>
2020-03-26 23:09:54 +0100

    Fix some stuff

--- whiskybase/spiders/whiskies.py
+++ whiskybase/spiders/whiskies.py
@@ -4,10 +4,25 @@ from pathlib import Path
 from pprint import pprint
 from itertools import islice
 from scrapy.crawler import CrawlerProcess
+from bs4 import BeautifulSoup
+import re
 
 def strip(text):
     if text:
         return text.strip()
+    else:
+        return None
+
+def text(e):
+    return strip(BeautifulSoup(e.get()).get_text())
+
+def extract_price(text):
+    if text:
+        match = re.search('([0-9,.]+)', text)
+        if match:
+            return match.group(1)
+        else:
+            return None
     else:
         return None
 
@@ -26,6 +41,7 @@ class Whisky(Item):
     rating = Field()
     average_price = Field()
     lower_price = Field()
+    bottler = Field()
 
 class WhiskiesSpider(Spider):
     name = 'whiskies'
@@ -38,18 +54,19 @@ class WhiskiesSpider(Spider):
 
     def parse_price(self, response):
         whisky = response.meta["whisky"]
-        whisky["average_price"] = strip(response.css(".block-shopping .block-price").xpath("p[2]/text()").get())
-        whisky["lower_price"] = strip(response.css(".block-shopping .block-shoplinks").xpath("p[3]/text()").get())
-        print(whisky)
+        whisky["average_price"] = extract_price(strip(response.css(".block-shopping .block-price").xpath("p[2]/text()").get()))
+        whisky["lower_price"] = extract_price(strip(response.css("#panel-shoplinks .wb--shop-links-panel--price").xpath("text()").get()))
+        bottler = text(response.css(".block-desc dl").xpath("dd[3]"))
+        whisky["bottler"] = bottler
         yield whisky
 
     def parse(self, response):
-        for tr in islice(response.css("table.whiskytable tbody tr"), 10):
+        for tr in islice(response.css("table.whiskytable tbody tr"), 20):
             url = strip(tr.xpath("td[3]/a/@href").get())
             whisky = Whisky(
                 url = url,
                 name = strip(tr.xpath("td[3]/a/text()").get()),
-                serie = strip(tr.xpath("td[3]/span/text()").get()),
+                serie = text(tr.xpath("td[3]/a/span")),
                 stated_age = strip(tr.xpath("td[4]/text()").get()),
                 strength = strip(tr.xpath("td[5]/text()").get()),
                 size = strip(tr.xpath("td[6]/text()").get()),
